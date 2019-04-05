b61fe1c9f1a2eb8bd0fb7bd2d35e4445ac13f793
matemolnar88 <matemolnar88@gmail.com>
2019-04-05 06:04:32 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Default DevCorner JavaScript Setting
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 40){
+      $('#topBtn').fadeIn();
+    } else{
+      $('#topBtn').fadeOut();
+    }
+  });
+
+  $("#topBtn").click(function(){
+    $('html ,body').animate({scrollTop : 0},500);
+  });
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+Html, Css and jQuery based Scroll Top Button website - exercise
+---
+
+![ScrollTopButton_jQuery](https://github.com/r4nd3l/ScrollTopButton_jQuery/blob/master/img/sample.gif)
